1cab486b57e819abadc6015596675d02d4cd2f09
Toffee <danbrierly@gmail.com>
2023-03-02 14:31:56 +0000

    First!

---
+++ QuickOpenFirstFolder.py
@@ -0,0 +1,20 @@
+import os
+import sublime
+import sublime_plugin
+
+items = []
+
+class PopupFirstFolderCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		items.clear()
+		folders = self.window.folders()
+		if len(folders) > 0:
+			for root, subdirs, files in os.walk(folders[0]):
+				for file in files:
+					items.append([file, root + "/" + file])
+		self.window.show_quick_panel(items, on_select=self.done)
+		
+	def done(self, result):
+		print(result)
+		if result >= 0:
+			self.window.open_file(items[result][1])
